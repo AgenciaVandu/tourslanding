ead6c1f990c3dd95b00a1a573c2d63a236e3120d
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2023-01-30 16:33:44 -0600

    Cambios

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -50,6 +50,8 @@ class TourController extends Controller
         Mail::to('agnes.jimenez@pronatura-sur.org')->send(new TourRegister($tour));
         Mail::to('eduardo.ramirez@yucatan.gob.mx')->send(new TourRegister($tour));
         Mail::to('sharon.reyes@yucatan.gob.mx')->send(new TourRegister($tour));
+        Mail::to('cecilia@beyondyucatan.travel')->send(new TourRegister($tour));
+
         /* Mail::to('ripmarenko@gmail.com')->send(new TourRegister($tour));
         Mail::to('ab@agenciavandu.com')->send(new TourRegister($tour)); */
 
